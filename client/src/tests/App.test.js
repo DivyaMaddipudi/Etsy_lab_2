22e7964c94f9d5bd55e59b9c3ae3bb99be9cb48a
DivyaMaddipudi <divya.maddipudi1@gmail.com>
2022-04-12 19:52:01 -0700

    Frontend testing added

--- client/src/tests/App.test.js
+++ client/src/tests/App.test.js
@@ -1,3 +1,19 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import register from "../components/register";
+import Dashboard from "../components/Dashboard";
+
+describe("Home Page", () => {
+  test("Opens home page and check data", () => {
+    render(<Dashboard />);
+    const text = screen.getByText(
+      /Explore one-of-a-kind finds from independent makers/i
+    );
+    expect(text);
+  });
+});
+
+test("Opens dashboard page", () => {
+  render(<Dashboard />);
+  const text = screen.getByText(/Home Decor/i);
+  expect(text);
+});
